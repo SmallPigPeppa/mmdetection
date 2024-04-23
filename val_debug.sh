ad00cd0fced26dc35ea1d11178e2d7941ea2114d
lwz <905328134@qq.com>
2024-04-24 00:18:15 +0800

    initial

--- val_debug.sh
+++ val_debug.sh
@@ -1,2 +1,11 @@
+#CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294.pth
+#CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e.py
+#WORK_DIR=work_dirs/vitdet_1024x1024
+#python tools/test.py ${CHECKPOINT_PATH} ${CHECKPOINT_PATH} --work_dir ${WORK_DIR}
+
+
+
 CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294.pth
-python tools/test.py projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e.py ${CHECKPOINT_PATH}
+CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e-512x512.py
+WORK_DIR=work_dirs/vitdet_512x512
+python tools/test.py ${CHECKPOINT_PATH} ${CHECKPOINT_PATH} --work_dir ${WORK_DIR}
