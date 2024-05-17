055069f97cee6c8b2129a439e1479aa6e316131b
lwz <905328134@qq.com>
2024-05-17 22:46:36 +0800

    initial

--- val_debug_pi.sh
+++ val_debug_pi.sh
@@ -17,7 +17,7 @@ WORK_DIR=work_dirs/vitdet_512x512_pi
 python tools/test.py ${CONFIG_PATH} ${CHECKPOINT_PATH} --work-dir ${WORK_DIR}
 
 
-#CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294_256x256_pi.pth
-#CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e-256x256.py
-#WORK_DIR=work_dirs/vitdet_256x256_pi
-#python tools/test.py ${CONFIG_PATH} ${CHECKPOINT_PATH} --work-dir ${WORK_DIR}
+CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294_256x256_pi.pth
+CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e-256x256-pi.py
+WORK_DIR=work_dirs/vitdet_256x256_pi
+python tools/test.py ${CONFIG_PATH} ${CHECKPOINT_PATH} --work-dir ${WORK_DIR}
