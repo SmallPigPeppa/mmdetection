99934d1ac45e075f6eb73b0370bb07ae18cb4a4f
lwz <905328134@qq.com>
2024-05-18 02:51:53 +0800

    initial

--- val_debug_bi.sh
+++ val_debug_bi.sh
@@ -1,4 +1,11 @@
-CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294_256x256_bi.pth
-CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e-256x256-pinew.py
-WORK_DIR=work_dirs/vitdet_256x256_bi
+#CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294_256x256_bi.pth
+#CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e-256x256-pinew.py
+#WORK_DIR=work_dirs/vitdet_256x256_bi
+#python tools/test.py ${CONFIG_PATH} ${CHECKPOINT_PATH} --work-dir ${WORK_DIR}
+
+
+
+CHECKPOINT_PATH=ckpt/vitdet_mask-rcnn_vit-b-mae_lsj-100e_20230328_153519-e15fe294_512x512_bi.pth
+CONFIG_PATH=projects/ViTDet/configs/vitdet_mask-rcnn_vit-b-mae_lsj-100e-512x512-pinew.py
+WORK_DIR=work_dirs/vitdet_512x512_bi
 python tools/test.py ${CONFIG_PATH} ${CHECKPOINT_PATH} --work-dir ${WORK_DIR}
